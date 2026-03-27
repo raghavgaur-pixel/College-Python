f5ed8bf7bb6ed6ff6fa1490de6fd7a1196a58fcd
Raghav Gaur <rgflamers.s.gaur@gmail.com>
2026-03-27 16:33:42 +0530

    OOPS

--- OOPS/oops.cpp
+++ OOPS/oops.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+
 using namespace std;
 class person
 {
@@ -9,3 +10,25 @@ class person
         void getdata(void);
         void display(void);
 };
+
+void person :: getdata(void)
+{
+        cout << "Enter Name: ";
+        cin >> name;
+        cout << "Enter Age: ";
+        cin >> age;
+}
+void person::display(void)
+{
+    cout << "\nName: " << name;
+    cout << "\nAge: " <<age;
+}
+
+int main()
+{
+    person p;
+    p.getdata();
+    p.display();
+
+    return 0;
+}
