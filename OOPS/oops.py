4bcea015fbe75c8799b8129377107717234ac8f4
Raghav Gaur <rgflamers.s.gaur@gmail.com>
2026-03-28 15:06:42 +0530

    Editted Some Stuff

--- OOPS/oops.py
+++ OOPS/oops.py
@@ -1,5 +1,30 @@
-class ABC:
+'''class ABC:
     var = 10
 
 obj = ABC()
-print(obj.var)
+print(obj.var)'''
+
+
+'''class Sample:
+    x,y = 10,20
+
+S = Sample()
+
+print("Value of x = ", S.x)
+print("Value of y = ", S.y)
+print("Value of x + y = ", S.x+S.y)'''
+
+class Student:
+    mark1,mark2,mark3 = 45,91,71
+
+    def process(self):
+        sum = Student.mark1 + Student.mark2 + Student.mark3
+        avg = sum/3
+
+        print("Total Marks = ", sum)
+        print("Average Marks = ", avg)
+
+        return
+    
+S = Student()
+S.process()
