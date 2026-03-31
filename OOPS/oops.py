2e25eeb18a3851ba3494302d68034ed139411f0b
Raghav Gaur <rgflamers.s.gaur@gmail.com>
2026-03-31 10:31:00 +0530

    Some Special Methods

--- OOPS/oops.py
+++ OOPS/oops.py
@@ -47,7 +47,7 @@ del obj1
 del obj2
 del obj3'''
 
-class Sample:
+'''class Sample:
     num = 0
     def __init__(self,var):
         Sample.num+=1
@@ -60,4 +60,27 @@ class Sample:
 
 S1 = Sample(15)
 S2 = Sample(35)
-S3 = Sample(45)
+S3 = Sample(45)'''
+
+class ABC():
+    def __init__(self, name, var):
+        self.name = name
+        self.var = var
+    def __repr__(self):
+        return repr(self.var)
+    def __len__(self):
+        return len(self.name)
+    def __cmp__(self, obj):
+        return self.var - obj.var
+    
+obj = ABC("abcdef", 10)
+print("The value stored in object is :", repr(obj))
+print("The length of the name stored in object is :", len(obj))
+obj1 = ABC("ghijkl", 1)
+val = obj.__cmp__(obj1)
+if val == 0:
+    print("Both values are equal")
+elif val == -1:
+    print("First value is less than second")
+else:
+    print("The second value is less than first")
