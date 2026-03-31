7f589fa0395d8a7713c46b2db2128d2fcf140e2f
Raghav Gaur <rgflamers.s.gaur@gmail.com>
2026-03-31 10:07:37 +0530

    __del__ Addition.

--- OOPS/oops.py
+++ OOPS/oops.py
@@ -14,7 +14,7 @@ print("Value of x = ", S.x)
 print("Value of y = ", S.y)
 print("Value of x + y = ", S.x+S.y)'''
 
-class Student:
+'''class Student:
     mark1,mark2,mark3 = 45,91,71
 
     def process(self):
@@ -27,4 +27,22 @@ class Student:
         return
     
 S = Student()
-S.process()
+S.process()'''
+
+class ABC():
+    class_var = 0
+    def __init__(self,var):
+        ABC.class_var += 1
+        self.var = var
+        print("The Object value is : ", var)
+        print("The value of class variable is : ", ABC.class_var)
+    def __del__(self):
+        ABC.class_var -= 1
+        print("Object with value", self.var, " is going out of scope.")
+
+obj1 = ABC(10)
+obj2 = ABC(20)
+obj3 = ABC(30)
+del obj1
+del obj2
+del obj3
