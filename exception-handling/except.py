edf9a13de4f8271e5553ae08ab7163ce1e90fd30
Raghav Gaur <rgflamers.s.gaur@gmail.com>
2026-04-10 16:50:35 +0530

    Stuff related to exceptions.

---
+++ exception-handling/except.py
@@ -0,0 +1,46 @@
+'''try:
+    num = int(input("Enter the number : "))
+    print(num**2)
+except (KeyboardInterrupt, ValueError, TypeError):
+    print("Please check before you enter............. Program Terminating")
+print("Bye!")'''
+
+'''import sys
+
+
+try:
+    file = pen('File1.txt')
+    str = f.readline()
+    print(str)
+except IOError:
+    print("Error occured during input.")
+except ValueError:
+    print("Could not convert data to an integer.")
+except:
+    print("Unexpected Error:", sys.exc_info()[0])'''
+
+'''try:
+    file = open('File1.txt')
+    str = file.readline()
+    print(str)
+
+except IOError:
+    print("Error occurred during Input.")
+else:
+    print("Program Terminating Succesfully")'''
+
+'''try:
+    file = open('File1.txt')
+    str = file.readline()
+    print(str)
+except:
+    print("Error occured")
+else:
+    print("Program Terminating Succesfully")'''
+
+try:
+    num = 10
+    print(num)
+    raise ValueError
+except ValueError:
+    print("Value Error Occured")
